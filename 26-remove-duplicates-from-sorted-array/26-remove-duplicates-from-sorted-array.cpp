7c8d561b3f97e7a793cefdffd6e904e2d0bc3ad9
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2022-03-17 10:34:05 +0530

    Update 26-remove-duplicates-from-sorted-array.cpp

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,3 +1,8 @@
+/*
+idea => using two pointers approach since the array is sorted keeping the first pointer which points only the unique elements since the unique element
+whereas the current pointer moves when it finds the unique pointer element is not equal to the current pointer element it swaps by first shifting the unique
+pointer and after swaping then shifting the current_pointer too
+*/
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
