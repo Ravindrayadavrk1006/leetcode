86e30aff1dde86fd8869a36cdb34d96fada21145
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2022-03-17 12:53:26 +0530

    Time: 3 ms (63.96%), Space: 8.9 MB (36.18%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,3 +1,8 @@
+/* approach=> two pointers approach
+    idea=>p_1 will find the <val> the elements to remove from between and p_2 will traverse till last ,   once the p_1 has reached the <val> element  and p_2 finds a <non_val> element
+then we will swap both and increase the position of p_1 too so that go for next swap of remaining val
+******* take a simple example and traverse the code
+*/
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
