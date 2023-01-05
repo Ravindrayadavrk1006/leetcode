5ac52ce875fe43db519c43de3cec86502cbd1bb9
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2023-01-05 09:06:48 +0530

    Create stone_game.cpp

---
+++ STONE-GAME/stone_game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    //since the player can start from either start or end and since the possibility of either one or two greater 
+  is always there and told there can't be draw which state that alice always win
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
