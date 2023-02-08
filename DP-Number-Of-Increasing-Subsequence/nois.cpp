114b49a35880cad1f1814ebd9aff94a035a5b37e
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2023-02-08 10:58:57 +0530

    check it once

--- DP-Number-Of-Increasing-Subsequence/nois.cpp
+++ DP-Number-Of-Increasing-Subsequence/nois.cpp
@@ -14,12 +14,14 @@ public:
                  if(nums[j]<nums[i] && lis[j]+1>lis[i])
                 {
                     lis[i] = lis[j]+1;
-                    //if increasing this means the count to reach i will be same as the counts to reach j since we have reached to j and since nums[i]>nums[j] hence we can reach here too
+                    //if increasing this means the count to reach i will be same as the counts to reach j since we have reached to 
+                      //j and since nums[i]>nums[j] hence we can reach here too
                     counts[i] = counts[j];
                     max_final_val= max(max_final_val,lis[i]);
                 }
 
-                //if the value lis[j]+1==lis[i]  is same this says we can have found a new way to reach current i and hence this new way no of ways to reach this j , will be added to the earlier no of ways we can reach here 
+                //if the value lis[j]+1==lis[i]  is same this says we can have found a new way to reach current i and hence 
+                 //this new way no of ways to reach this j , will be added to the earlier no of ways we can reach here 
                 else if(nums[j]<nums[i] && lis[j]+1==lis[i])
                 {
                     counts[i]+=counts[j];
