7e1c7c3a58aa980df672fbeca7d11f25414ca7cf
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2023-02-21 21:31:54 +0530

    concept of front parition and similar to palindrome partition

--- DP-Partition Array for Maximum Sum/pafms.cpp
+++ DP-Partition Array for Maximum Sum/pafms.cpp
@@ -10,6 +10,14 @@ private:
     here we can have (1{15,7,9,2,5,10}), (1,15{7,9,2,5,10}),(1,15,7{9,2,5,10})
     we can have as the above parition and the {} need to be solved recursively
     */
+    /*
+    # Complexity
+- Time complexity:
+O(n*k)
+
+- Space complexity:
+O(n+n)
+*/
     int sol(int i,vector<int>& arr, int k,int n , vector<int>& dp)
     {
         if(i==arr.size())return 0;
@@ -34,9 +42,39 @@ private:
         return dp[i] = max_val;
 
     }
+ int tabulation(vector<int>& arr, int k )
+    {
+        int n = arr.size();
+        vector<int> dp(n+1,0);
+        //1.copy the base case
+        dp[n] = 0 ;
+        //2. reverse run the loop of that in the recursion dp i.e in recursive i-> 0 to n-1 here n-1 to 0 
+        for(int i = n-1 ; i>=0 ; i--)
+        {
+            //3.copy paste the remaining code from the recursive dp
+
+             int max_val = -1e9;
+        //keeping track of max value and length till now since we have to do according to question max_val_of_current_parition*lengthof_current_parition
+        int length_of_current_parition = 0;
+        int max_val_of_current_parition = -1e9; 
+        for(int j = i; j<min(n,i+k) ; j++)
+        {
+            length_of_current_parition++;
+            max_val_of_current_parition= max(max_val_of_current_parition,arr[j]);
+            int curr_val = length_of_current_parition*max_val_of_current_parition + dp[j+1];
+            max_val = max(max_val , curr_val);
+        }
+        dp[i] = max_val;
+        }
+        return dp[0];
+    }
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(),-1);
-        return sol(0,arr,k,arr.size(),dp);
+        // vector<int> dp(arr.size(),-1);
+        // return sol(0,arr,k,arr.size(),dp);
+
+
+        //calling the tabulation approach
+        return tabulation(arr,k);
     }
 };
