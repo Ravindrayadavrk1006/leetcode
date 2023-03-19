c9368fd1248dd4ddacaefca390076615df6c241a
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2023-03-19 21:24:53 +0530

    important concept of maximize the minimum

--- Arrays-29-[Binary-Search]-Aggressive Cows/ac.cpp
+++ Arrays-29-[Binary-Search]-Aggressive Cows/ac.cpp
@@ -2,6 +2,18 @@ explained well in C.P notebook
 problem link --->  https://www.codingninjas.com/codestudio/problems/aggressive-cows_1082559?leftPanelTab=0
 
 
+leetcode problem on this concept -->
+    
+1552. Magnetic Force Between Two Balls
+1870. Minimum Speed to Arrive on Time
+875. Koko Eating Bananas
+1011. Capacity To Ship Packages Within D Days
+1283. Find the Smallest Divisor Given a Threshold
+1482. Minimum Number of Days to Make m Bouquets
+2064. Minimized Maximum of Products Distributed to Any Store
+774 Minimize Max Distance to Gas Station
+410 Split Array Largest Sum
+
 /*
     naive approach -->
     here we try to start with taking a dist of 1 and try to increase 
