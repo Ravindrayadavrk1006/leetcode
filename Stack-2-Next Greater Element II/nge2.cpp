2eda770d915d26d9b37a2b1c854739323195857b
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2023-04-01 11:16:45 +0530

    good concept of stack next greater element 1

--- Stack-2-Next Greater Element II/nge2.cpp
+++ Stack-2-Next Greater Element II/nge2.cpp
@@ -38,8 +38,8 @@ private:
     {
         int n = nums.size();
         vector<int> new_vec;
-        for(int i = 0; i<n; i++)new_vec.push_back(nums[i]);
-        for(int i = 0; i<n; i++)new_vec.push_back(nums[i]);
+        // for(int i = 0; i<n; i++)new_vec.push_back(nums[i]);
+        // for(int i = 0; i<n; i++)new_vec.push_back(nums[i]);
         int new_vec_size = 2*n;
         vector<int> ans(2*n,-1);
         stack<int> st;
@@ -56,10 +56,38 @@ private:
         ans_final.assign(ans.begin(),ans.begin()+n);
         return ans_final;
     }
+    //same above approach a bit cleaner and more optimized
+    /*
+        we will use modules to get value above n 
+        suppose array 1 2 1 
+        n =3 for repeating element for getting element at 4th index place we do 4%3 i.e we get 1 index i.e 2 
+        also we don't need to store values for above n 
+    */
+    vector<int> cleaner_approach(vector<int> & nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        int traverse = 2*n ;
+        stack<int> st;
+        for(int i = traverse-1; i>=0; i--)
+        {
+               while(!st.empty() && st.top()<=nums[i%n])st.pop();
+               if(i<n)
+               {
+                    if(!st.empty())
+                    {
+                        ans[i]=st.top();
+                    }
+               }
+               st.push(nums[i%n]);
+        }
+        return ans;
+    }
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         // return sol(nums);
 
-        return stack_approach(nums);
+        // return stack_approach(nums);
+        return cleaner_approach(nums);
     }
 };
