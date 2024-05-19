dc314d9f9c7362f659319c572b89550020254db5
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2024-05-19 17:08:13 +0530

    core concept -> dijkstra algorithm two approach , priority queue and set approach

--- Graphs/21. [*coding-ninja]- Dijkstra's shortest path.cpp
+++ Graphs/21. [*coding-ninja]- Dijkstra's shortest path.cpp
@@ -1,3 +1,4 @@
+//https://www.naukri.com/code360/problems/dijkstra-s-shortest-path_920469?leftPanelTabValue=SUBMISSION
 #include <bits/stdc++.h> 
 /*
     it is normal bfs travesal with one point in head that using min-heap or priority queue instead of normal queue.
@@ -5,7 +6,8 @@
     apart from this everything is exactly the bfs travesal
 
 */
-vector<int> sol_dijkstra_algo(vector<vector<int>> &vec, int vertices, int edges, int source){
+//*********SOL 1 -- >USING PRIORITY QUEUE
+vector<int> sol_dijkstra_algo_using_priority(vector<vector<int>> &vec, int vertices, int edges, int source){
     //creating adj_list from given vertices
     vector<vector<pair<int,int>>> adj_list(vertices);
     for(auto it: vec){
@@ -38,7 +40,59 @@ vector<int> sol_dijkstra_algo(vector<vector<int>> &vec, int vertices, int edges,
     }
     return dist;
 }
+//------------------------------------------------ SOL 2--------------
+
+vector<int> sol_dijkstra_algo_using_set(vector<vector<int>> &vec, int vertices, int edges, int source){
+    //creating adj_list from given vertices
+    vector<vector<pair<int,int>>> adj_list(vertices);
+    for(auto it: vec){
+        adj_list[it[0]].push_back({it[1],it[2]});
+        adj_list[it[1]].push_back({it[0],it[2]});
+    }
+    // creating priority queue or min-heap in c++
+    // telling the value is pair<int,int>, second is saying about all the
+    // values, and third is compareter function
+    // the value will be stored in form { 4,1} i.e 4 is the min dist found till now to reach 1 from starting point i.e 0
+    //the storing order is {dist,node} so that min-heap is formed on basis of dist first i.e min distance at top, if min distance is same then it will try sorting on basis of node , which is not required also but distance sorting is required and important
+   set<pair<int,int>> st;
+    vector<int> dist(vertices,INT_MAX);
+    //since given that starting point will always be zero so let's mark to reach from 0 to 0 we need 0 distance
+    dist[source] = 0;
+    st.insert({0,source});
+    while(!st.empty()){
+        //** important how we are getting smallest value from the set
+        auto front = *(st.begin());
+        int node = front.second;
+        int distance = front.first;
+        //erase the elemnt from the set
+        st.erase(front);
+        //important point to note in adj_list pairs elmnt order {node,distance}
+        for(auto it: adj_list[node]){
+            //dist to this adj_node from the current parent i.e variable node
+            int dist_to_node =  dist[node]+it.second;
+            if(dist_to_node < dist[it.first]){
+                //**SLIGHT BETTER IMPROVEMENT ALSO NOT REQUIRED IF FEELING TO HARD
+                //we are trying to remove the previous reachable to current node , if it already been visited it means it means
+                //it would have taken more distance than current
+                //it would be of no use to try to reach to other node from this it node  which is reachable by smaller distance , so let's delete it's previous occurance if present in the set
+                //deleting will avoid us trying few iterations 
+                // if(dist[it.second] != INT_MAX){  //this alone will say that yes this node has already been visited  by someone else
+                //     st.erase({dist[it.first],it.second});
+                // }
+                dist[it.first] =dist_to_node;
+                st.insert({dist_to_node,it.first});
+            }
+        }
+    }
+    return dist;
+}
+
+
 vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int edges, int source) {
-   return sol_dijkstra_algo(vec,vertices, edges,source);
+   //calling sol 1
+   //return sol_dijkstra_algo_using_priority(vec,vertices, edges,source);
+
+   //calling sol 2
+   return sol_dijkstra_algo_using_set(vec,vertices, edges,source);
 
 }
