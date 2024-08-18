9e02892c72f8c3583a43cb4861a925f206014167
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2024-08-18 11:07:38 +0530

    basic concept of linked list

---
+++ Linked-List/2.-CODING-NINJA-Insertion In A Singly Linked List.cpp
@@ -0,0 +1,40 @@
+/*
+    explanation-> 
+        in linked list always remember to handle the base test cases
+            1. if the head is null then the node can be inserted at 0th position only
+            2. if to insert at 0th position i.e head then we create a node and make it as head and 
+            3. if inserting at any other position 1 to ...last
+                we normally stand at one postion behind and check if this position is counter = pos-1 if so we insert at that position
+
+*/
+Node * insert(Node * head, int n, int pos, int val) {
+    if(head == NULL){
+        //if the head is null then the node can be inserted at index 0 only
+        if(pos == 0){
+            Node* temp = new Node(val);
+            return temp;
+        }
+    }
+    //inserting at start
+    if(pos == 0){
+        Node* temp = new Node(val);
+        temp->next = head;
+        return temp;
+    }
+    //if inserting at any other index i.e 1 or further
+    int counter = 0;
+    Node* traverser = head;
+    //if the node has not reached the null 
+    while(traverser){
+        if(counter == pos-1){
+            Node* temp = new Node(val);
+            //this line is done first so that we don't loose reference for traverser->next
+            temp->next = traverser->next;
+            traverser->next = temp;
+            break;
+        }
+        counter++;
+        traverser = traverser->next;
+    }
+    return head;
+}
