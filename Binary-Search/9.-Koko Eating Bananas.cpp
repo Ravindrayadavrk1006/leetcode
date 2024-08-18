3f9798c2e285a3caf6349d3ef3d71ab4c62ac716
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2024-08-18 08:48:06 +0530

    application of minimize the maximum, since he likes to eat slow

---
+++ Binary-Search/9.-Koko Eating Bananas.cpp
@@ -0,0 +1,49 @@
+exactly same as Arrays-30-[Binary-Search]-Minimum Speed to Arrive on Time
+
+
+class Solution {
+private:
+
+    //finding the time if possible to eat in that much of time
+    bool possible_to_reach(vector<int>& arr,int mid, int st)
+        {
+            double t = 0;
+            double max_time = (double)st;
+            int n = arr.size();
+            for(int i =0; i<n-1; i++)
+            {
+                //since in that hour he will take the entire hour and wait if there is time remaining in that hour
+                t+=ceil(1.0*arr[i]/mid);
+                if(t>max_time)return false;
+            }
+            t+=(1.0*arr[n-1]/mid);
+            if(t<=max_time)return true;
+            return false;
+        }
+    int BS(vector<int>& arr,int st)
+        {
+            //defining the search space
+            //minimum interger speed 
+            int left = 1;
+            
+            //tried to take a max value here our test cases are sufficed by INT_MAX/2  
+            int right =  INT_MAX/2;
+            while(left<=right)
+            {
+                int mid = (left+right)/2;
+                if(possible_to_reach(arr,mid,st))
+                {
+                    right = mid-1;
+                }
+                else
+                {
+                    left = mid+1;
+                }
+            }
+            return left;
+        }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        return BS(piles,h);
+    }
+};
