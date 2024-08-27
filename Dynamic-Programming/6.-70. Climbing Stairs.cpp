2a696a74c25f441271ea4cff4dfd2b6c4ae4a215
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2024-08-27 09:01:26 +0530

    Create 6.-70. Climbing Stairs.cpp

---
+++ Dynamic-Programming/6.-70. Climbing Stairs.cpp
@@ -0,0 +1,35 @@
+INTUITION
+Idea is using DP and the solution is same as fibonacci series.
+Idea is climbing the staircases is coming down
+hence used the same approach
+
+APPROACH
+stored memo[0]=1 since coming to zero from previous step either by taking one step or two step has just one way.
+Starting from n and taking one and two step in each cases coming down.
+
+*** took bottom up approach
+
+Complexity
+Time complexity:
+O(N)
+
+Space complexity:
+O(N)
+
+Code
+class Solution {
+private:
+    int climb_dp(int n, vector<int>& dp){
+        if(n==0)return 1;
+        if(n<0)return 0;
+        //if the value already exist return it
+        if(dp[n] != -1)return dp[n];
+        //save in the dp array before returning
+        return dp[n] = climb_dp(n-1, dp) + climb_dp(n-2, dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return climb_dp(n, dp);
+    }
+};
