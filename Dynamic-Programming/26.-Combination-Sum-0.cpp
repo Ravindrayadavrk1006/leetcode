87ac89dedc1036c1212ac993c7f4c4fa366345a4
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2024-09-03 07:19:54 +0530

    another approach of finding all permutation with repeated intake of values

---
+++ Dynamic-Programming/26.-Combination-Sum-0.cpp
@@ -0,0 +1,45 @@
+# Intuition
+since have to find all the permutation which give the required values
+we start initially our sum is target and try to reach sum 0 using the target -> 0 approach
+
+eg [1,2,3] and target = 4
+
+                               []
+                        /       \     \
+                       1        2      3
+                     / | \     / | \   / | \
+                    1  2  3   1  2  3  1  2 3
+we try to find when the sum becomes equal to required (note in the question opposite of this approach of used that is assumed we have given sum and we try to reach to 0 ) 
+this fulfill our idea of getting all the permutations too
+
+
+# Complexity
+- Time complexity:
+O(n)
+
+- Space complexity:
+O(n+n)
+
+# Code
+```
+class Solution {
+public:
+    int help(int target, vector<int>& nums, vector<int>& dp)
+    {
+        if(target<0)return 0;
+        if(target ==0)return 1;
+        if(dp[target] != -1)return dp[target];
+        int total_com = 0;
+        for(int i = 0 ; i<nums.size(); i++)
+        {
+            total_com+=help(target-nums[i],nums, dp);
+        }
+        return dp[target] = total_com;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1, -1);
+        return help(target,nums, dp);
+
+    }
+};
+```
