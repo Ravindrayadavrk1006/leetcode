578f4635efffb0bdc5b5e65a0cff1b047ed60c5e
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2024-09-13 09:05:51 +0530

    [**imp]-TYPE-CONCEPTS(many new concepts)-finding height of histogram

---
+++ Dynamic-Programming/59.-84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,77 @@
+class Solution {
+private:
+    /*
+        using example 1 of Problem Description
+        we can get the largest rectange of size 10 ie 5*2 i.e one given 5 and other from 6 size histogram
+
+        we can find the area of rectangle like :
+        standing at 2 size histgram we see we can get max of size 2 rectangle since next hist is of size 1  , standing at 1 size histogram we figure we can go in left direction till 0 index and in right we can go till 3 size histgram and we will get rectangle of size 6*1 i.e right pos-left pos * current size of histogram
+        similarly for 5 we can't go in left direction since privious hist is of size 1 and in right direction we can go since the size of histogram is 6 hence we get 2*5 size rectangle using the same concept 
+
+
+    for each histogram we can go in left and in right find the hist whose size is smaller than the current hist and we can do (right-left) *current hist size to get the rectangle 
+    same concept is being used in below solution
+
+    just to improve the time complexity of finding left and right small for each hist fromn
+    to constant we are using a concept which is explained below 
+    
+    
+    */
+
+
+    int sol(vector<int>& heights)
+    {
+        int n = heights.size();
+        stack<int> st;
+        int leftsmall[n];
+        int rightsmall[n];
+
+        //populating the left small array
+
+
+        //working:
+        /*
+            we take a stack and start traversing the given heights array 
+            the idea used is for the current heights[i] we remove all the elements which are greater or equal than the current heights[i] from the stack and in the leftsmall array store the element current_stack_stop+1 and then push the current element in the stack
+
+            take paper pen to visualize the working
+        */
+        for(int i = 0; i<n ; i++)
+        {
+            //removing all the elements from stack which are greater than the current element
+            while(!st.empty() && heights[st.top()]>= heights[i])st.pop();
+            if(st.empty())leftsmall[i] = 0;
+            else leftsmall[i]= st.top()+1;
+            st.push(i);
+        }
+
+        //making sure the stack is empty for again use
+        while(!st.empty())st.pop();
+        //populating the right small array
+        //same way we find the left small
+        for(int i = n-1; i>=0 ; i--)
+        {
+            //removing all the elements from stack which are greater than the current element
+            while(!st.empty() && heights[st.top()]>= heights[i])st.pop();
+            if(st.empty())rightsmall[i] = n-1;
+            else rightsmall[i]= st.top()-1;
+            st.push(i);
+        }
+
+
+        //finding the heights of each rectangle 
+        //formula used (rightsmall[i]-leftsmall[i] +1)*height of ith histogram
+
+        int max_size_rectangle = -1e9;
+        for(int i = 0 ; i<n ; i++)
+        {
+            int curr_rec_size=  (rightsmall[i]-leftsmall[i]+1)*heights[i];
+            max_size_rectangle = max(max_size_rectangle,curr_rec_size);
+        }
+        return max_size_rectangle;
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        return sol(heights);
+    }
+};
