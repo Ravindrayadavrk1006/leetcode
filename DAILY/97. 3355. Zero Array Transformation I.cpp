f82c5671d37c429f57fdb38588a0431d8bb2241e
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2025-05-20 20:45:22 +0530

    [**IMP] Topics 1. Line Seep Algorithm
    1. range query perfect fit for line sweep algorithms

---
+++ DAILY/97. 3355. Zero Array Transformation I.cpp
@@ -0,0 +1,112 @@
+class Solution {
+private:  
+    /*
+
+        brute force approach ->
+
+            we are doing noting but running over all the queries and in a reduction arr storing the values to be substacted for each index and finally substracting giving TLE 
+
+        time complexity -> O(n*k) where at max (n*n) i.e 10^5*10^5 which emplies that n^2 not good enough
+    
+    */
+
+    //GIVING TLE
+    bool brute_force(vector<int>& nums, vector<vector<int>>& queries){
+        int n = nums.size();
+        int k = queries.size();
+        vector<int> reduction_arr(n,0);
+        for(auto query: queries){
+            int start = query[0];
+            int end = query[1];
+            for(int i = start; i<=end; i++){
+                reduction_arr[i]++;
+            }
+        }
+        //now we have the reduction array maintained let's try to decremenet
+        for(int i =0; i<n; i++){
+            if(nums[i]- reduction_arr[i] >0)return false;
+        }
+
+
+        return true;
+    }
+    /*
+        using line sweep algorithm->
+
+
+            line sweep algorithm ->  we are taking each query which is a range ->
+            we are inserting the query[0], +1  -> +1 denotes it's the starting of the query, we are inserting query[1] + 1 , -1  -> -1 denoting it's a end of a query  ,  also +1 is being done because according to the question till current element it will be considered but not from next element because according to the question [1,3] represent the line is from 1 till 3 after 3 the line won't be there and since the point are integer hence query[1]+1   therefore that (+1) is done.
+
+
+
+            once we have inserting, we do the sorting (normal line sweep)
+
+            then we take a element called current which will calcuate the current value
+            and we travese the elements of line_sweep sorted vector
+            we keep on updating the current vector and also marking in a vector called range_substraction the boundary value i.e pair.first with the current value
+
+            range_substraction vector is saying that where till non-zero next value current value will be considered
+
+            eg [2,0,0, 4, 0,0, 1]  -> from index 0 to 2  -> 2 value will be considered , from index 3 to 5 -> 4 value will be considered and on 6 -> 1 value will be considered.
+
+            we are filling same thing i.e above vector making it [2,2,2,4,4,4,1] in the next for loopp i.e STEP 4
+
+
+            in the STEP 5 we are substracting from the nums array and if at any point we find the value after substraction is greater than 0 then we return false else return true;
+    
+    
+    */
+    bool using_line_sweep(vector<int>& nums, vector<vector<int>>& queries){
+        int n = nums.size();
+        int q = queries.size();
+        vector<pair<int,int>> line_sweep;
+        //STEP 1
+        for(auto query: queries){
+            line_sweep.push_back({query[0], 1});
+            line_sweep.push_back({query[1]+1, -1});
+        }
+
+        //STEP 2
+        //sorting the line sweep vector
+        sort(line_sweep.begin(), line_sweep.end());
+        // unordered_set<int> lines_st;
+        vector<int> range_substraction(n+1,0);
+        int curr = 0;
+
+        //STEP 3
+        for(auto el: line_sweep){
+            int line_pos = el.first;
+            curr+=el.second;
+            range_substraction[line_pos] = curr;
+        }
+
+        //STEP 4
+        //run a for looop and fill the range_substraction array
+        int filler = 0;
+        for(int i =0; i<n; i++){
+            if(range_substraction[i]== 0)range_substraction[i] = filler;
+            else{
+                filler = range_substraction[i];
+            }
+        }
+
+
+
+        //STEP 5
+
+        //we can run this for loop in the above for loop after updating the value of filler
+        for(int i =0; i<n; i++){
+            if(nums[i]- range_substraction[i]>0)return false;
+        }
+
+        return true;
+    }
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        //SOL 1 brute force
+        // return brute_force(nums, queries);
+
+
+        return using_line_sweep(nums, queries);
+    }
+};
