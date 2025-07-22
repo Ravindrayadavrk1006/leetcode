746846adce9aede67059fe1095ea31b527552c4b
ravindra <47749676+Ravindrayadavrk1006@users.noreply.github.com>
2025-07-22 09:11:43 +0530

    Topics 1. Sliding window  2. sub array

---
+++ DAILY/126. 1695. Maximum Erasure Value.cpp
@@ -0,0 +1,62 @@
+class Solution {
+    /*
+        HINT: sub array, which indicates towards sliding window protocol
+
+        APPROACH:
+            1. if the current element is already present in the current window then remove all the elements from start till the occurance of that element and from the sum keep substracting the removed value 
+            2. insert the current element and add it to the current sum.
+
+            3. we have to maximize the answer, hence taking an ans variable which will be maximized with the current sum.
+
+
+        
+        CODE:
+            we will be using queue and set, and a variable sum which will store the current valid subarray sum and ans variable which will be storing the maximum value of sum.
+            1. Traverse for all elements of the array
+            2. If current element is already present,  from the queue start remove all element upto the current element, at the same time keep removing the element from the set as well and substracting from the sum since the element is being removed
+            3. Insert the current element and increase the sum
+            4. Try maximizing the ans with current sum value
+
+
+    */
+private:
+    int sol(vector<int>& nums ){
+        int n = nums.size();
+        //inserting the elment so that the order is maintained and since the elements will be removed from the front and hence easy DS choice 
+        queue<int> q;
+        //for checking if current element is already present in the queue or not
+        set<int> st;
+        // st.insert(nums[0]);
+        // q.push(nums[0])
+        int sum = 0;
+        int ans = 0;
+        for(int i =0; i<n; i++){
+            //if current element is al#ready present in the queue, remove all elements upto the first occurance of that element
+            //if current element is already present
+            if(st.find(nums[i]) != st.end()){
+                //remove all elements upto the  occurance of nums[i]
+                while(q.size()){
+                    int front = q.front();
+                    //remove from queue
+                    q.pop();
+                    //remove front stack
+                    st.erase(front);
+                    sum-=front;
+                    if(front == nums[i])break;
+                }
+            }
+            //insert the current element in set and queue
+            st.insert(nums[i]);
+            q.push(nums[i]);
+            //update the sum
+            sum+=nums[i];
+            //maximize the ans
+            ans = max(ans, sum);
+        } 
+        return ans;  
+    }
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        return sol(nums);
+    }
+};
