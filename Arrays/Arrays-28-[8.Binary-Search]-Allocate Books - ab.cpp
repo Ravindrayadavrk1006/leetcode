f581e1db36493b33903ab0c3f21e2d7d385ec251
Ravindra Yadav <ravindrayadav@Ravindras-MacBook-Pro.local>
2026-06-02 20:42:00 +0530

    refactored everything, the unrelated or ungrouped till now are in Ungrouped folder

---
+++ Arrays/Arrays-28-[8.Binary-Search]-Allocate Books - ab.cpp
@@ -0,0 +1,151 @@
+coding ninjas
+problem link ---> https://www.codingninjas.com/codestudio/problems/allocate-books_1090540?leftPanelTab=0
+
+--------------------   RECURSIVE AND DP APPROACH  --------------------
+#include <bits/stdc++.h> 
+using namespace std;
+int sol_rec(int pos , int st,vector<int>& arr,vector<vector<int>>& dp)
+{
+    if(st==1)
+    {
+        int sum = 0;
+        for(int i = pos; i<arr.size();i++)sum+=arr[i];
+        return sum;
+    }
+    if(dp[pos][st] != -1)return dp[pos][st];
+    //normal cases
+    int g_min = INT_MAX;
+    int first = 0;
+    for(int i =pos; i<=arr.size()-st; i++)
+    {
+        first = first+arr[i];
+        int second = sol_rec(i+1,st-1,arr,dp);
+        g_min = min(g_min,max(first,second));
+    }
+    return dp[pos][st] = g_min;
+}
+/*
+  binary seach approach explained well in notebook of cp  
+  
+  
+*/
+//binary search approach
+
+
+bool _allocation_possible(vector<int>& arr, int mid, int st )
+{
+    int temp_st = 0;
+    int pages = 0;
+    for(int i =0;i<arr.size(); i++)
+    {
+        //since a entire book has to be atleast allocated to a person
+        if(arr[i]+pages > mid)
+        {
+            pages = arr[i];
+            temp_st++;
+            if(pages>mid)return false;
+        }
+        else
+        {
+            pages+=arr[i];
+        }
+        
+    }
+    if(temp_st<st)return true;
+    return false;
+}
+int BS(vector<int>& arr, int n, int st)
+{
+    //defining the search space
+    int left = INT_MAX;
+    int right = 0;
+    for(int i = 0;i<n; i++)
+    {
+        left = min(left,arr[i]);
+        right = right+arr[i];
+    }
+    while(left<=right)
+    {
+        int mid = (left+right)/2;
+        if(_allocation_possible(arr,mid,st))
+        {
+            right = mid-1;
+        }
+        else
+        {
+            left = mid+1;
+        }
+    }
+    return left;
+}
+
+
+int allocateBooks(vector<int> arr, int n, int m) {
+  
+  //callling recursion approach 
+//     vector<vector<int>> dp(arr.size(),vector<int>(m+1,-1));
+//     return sol_rec(0,m,arr,dp);
+  
+  
+  //binary search approach--->
+//SOLUTION 1
+    // vector<vector<int>> dp(arr.size(),vector<int>(m+1,-1));
+    // return sol_rec(0,m,arr,dp);
+
+    //SOLUTION 2
+    return BS(arr,n,m);
+}
+
+
+
+
+//---------------------------------- in GFG ------------------
+//PROBLEM LINK-> https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+
+class Solution {
+  private:
+    bool _can_be_allocated(int n, int arr[], int m, int mid){
+        int pages = 0;
+        int curr_st = 0 ;
+        for(int i = 0 ; i<n ; i++){
+            if(pages + arr[i] > mid ){
+                pages = arr[i];
+                curr_st++;
+                if(arr[i]>mid)return false;
+            }
+            else{
+                pages+=arr[i];
+            }
+        }
+        //since count of students is starting from 0 ,if m number of students can be allocated properly then fine else not
+        if(curr_st < m)return true;
+        return false;
+    }
+  public:
+    // Function to find minimum number of pages.
+    long long findPages(int n, int arr[], int m) {
+        int low = INT_MAX;
+        int high = 0;
+        for(int i = 0 ; i<n; i++){
+            if(arr[i]<low)low = arr[i];
+            high+=arr[i];
+        }
+        if(m>n)return -1;
+        // cout<<"low"<<low<<" high"<<high<<endl;
+        //working on logic 
+        int ans = INT_MAX;
+        while(low<=high){
+            // cout<<"low"<<low<<" high"<<high<<endl;
+            int mid = (low+high)/2;
+            //if this much value can be allocated let's try for lower value
+            if(_can_be_allocated(n,arr, m, mid)){
+                // cout<<"can be allocated"<<mid<<endl;
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        // cout<<"mid"<<ans<<" high"<<high<< " low"<<low<<endl;
+        return low;
+    }
+};
---
+++ Arrays/Arrays-6-Maximum-Subarray - ms.cpp
@@ -0,0 +1,182 @@
+/*
+appraoch from striver 1 
+not the one used coded below
+O(n^3 ) complexity 
+we take three loop 
+i -> 0->n-1
+j-> i to n-1
+k runs from i to j to calculate the sum of this subarray
+
+the idea behind this approach we try to form all the subarray and callculate their sum and compare with a final_sum which is intialized to nums[0];
+appraoch from striver 2:
+    O(n^2)
+    since k is just used to calulate the sum we intialize sum = 0;
+    in ith loop and in jth loop for each run we just do sum+nums[j] and compare it with the final_sum value
+
+appraoch 3 from striver kadanes algorith:
+    O(n)
+    here we keep sum intialized to 0
+    and max_val = nums[0];
+    we start traversing and if the sum value become less than 0 we mkae sum =0 afte comparing it with max_val=max(max_val,sum)
+    we are updating to 0 since if the value is coming negative that won't be usefull in the coming subarray
+    and hence when the sum value becomes <0 then we say we won't be using this subarray le't try to form a new subarray with intial value 0
+
+*/
+
+
+
+
+
+class Solution {
+private:
+//appraoch 3 from striver kadanes algorithm:
+    int kadane_algo(vector<int>& nums)
+    {
+        int n = nums.size();
+        int sum = 0;
+        int max_val = nums[0];
+        for(int i = 0 ;i<n ; i++)
+        {
+            sum+=nums[i];
+            max_val = max(max_val,sum);
+            if(sum<0)sum = 0;
+        }
+        return max_val;
+    }
+public:
+
+    //approach 3 --> divide and conquer approach
+    int cross_sum(vector<int>& nums, int p, int q)
+    {
+        int mid=(p+q)/2;
+        int left_sum=0;
+        int left_best_sum=0; 
+        for(int i=mid;i >= p;i--)
+        {
+            left_sum+=nums[i];
+            if(left_sum > left_best_sum)
+            {
+                left_best_sum=left_sum;
+            }
+        }
+        //right side of cross sum
+        int right_sum=0;
+        int right_best_sum=0;
+        for(int i=mid+1;i<=q;i++)
+        {
+
+                right_sum +=nums[i];
+                if(right_sum > right_best_sum)
+                {
+                    right_best_sum=right_sum;
+                }
+        }
+        return right_best_sum+left_best_sum;
+    }
+    int help(vector<int>& nums, int p, int q)
+    {
+       if(p==q)return nums[p];
+       else
+       {
+           int mid=(p+q)/2;
+           int left_sum=help(nums,p,mid);
+           int right_sum=help(nums,mid+1,q);
+           int cross_sum_result=cross_sum(nums,p,q);
+           return max(left_sum,max(right_sum,cross_sum_result));
+       }
+        
+    }
+    int maxSubArray(vector<int>& nums) {
+        // int temp=0;
+        // int n=nums.size();
+        // return help(n,nums); 
+        //handling negative cases
+        int i=0;
+        while(i<nums.size())
+        {
+            if(nums[i]>=0)break;
+            i++;
+        }
+        if(i == nums.size() )return (*max_element(nums.begin(),nums.end()));
+        //calling the solution 3 Dviide and Conqueor
+        // else return help(nums,0,nums.size()-1);
+        //calling kadanes algo striver sol 3
+        else return kadane_algo(nums); 
+    }
+};
+
+
+
+
+
+//approach 2
+// class Solution {
+// public:
+//     int help(int n, vector<int>& nums)
+//     {
+//         int max_sum=nums[0];
+//         int curr_sum=nums[0];
+//         // int start_pos=0;
+//         int curr_pos=1;
+//         while(curr_pos<n)
+//         {
+//             // curr_sum+=nums[curr_pos];
+//             if(curr_sum>0)
+//             {
+//                 // start_pos=curr_pos+1;
+//                 curr_sum+=nums[curr_pos];
+//             }
+//             else
+//             {
+//                 curr_sum=nums[curr_pos];
+//             }
+//             if(curr_sum>max_sum)
+//             {
+//                 max_sum=curr_sum;
+//             }
+//             curr_pos++;
+//         }
+//         return max_sum;
+//         
+//     }
+//     int maxSubArray(vector<int>& nums) {
+//         int temp=0;
+//         int n=nums.size();
+//         return help(n,nums); 
+//     }
+// };
+//approach 1
+// class Solution {
+// public:
+//     int help(int n, vector<int>& nums)
+//     {
+//         int max_sum=0;
+//         int curr_sum=0;
+//         // int start_pos=0;
+//         int curr_pos=0;
+//         while(curr_pos<n)
+//         {
+//             curr_sum+=nums[curr_pos];
+//             if(curr_sum<=0)
+//             {
+//                 // start_pos=curr_pos+1;
+//                 curr_sum=0;
+//             }
+//             if(curr_sum>max_sum)
+//             {
+//                 max_sum=curr_sum;
+//             }
+//             curr_pos++;
+//         }
+//         return max_sum;
+//         
+//     }
+//     int maxSubArray(vector<int>& nums) {
+//         int temp=0;
+//         int n=nums.size();
+//         handling negative values very neive solution
+//         while(temp<n && nums[temp]<0)temp++;
+//         if(temp==n)return *max_element(nums.begin(), nums.end());
+//         return help(n,nums); 
+//     }
+// };
